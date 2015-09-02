fd17f2c4687603673776cb7cff00966a0cac592f
Ty Rauber <tyrauber@mac.com>
2015-09-02 16:46:28 -0700

    Fix geojson mime type. Version bump to 0.0.3

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,1 +1,1 @@
-Mime::Type.register "application/json", :geojson, %w( text/x-json application/jsonrequest )
+Mime::Type.register "application/geojson", :geojson, %w( text/x-json application/jsonrequest )
